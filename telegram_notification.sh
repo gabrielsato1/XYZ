0fb4be1699a62394b4a667aca69acc9934f0f42c
gabrielsato1 <142916841+gabrielsato1@users.noreply.github.com>
2023-09-15 15:53:46 -0300

    Create telegram_notification.sh

---
+++ telegram_notification.sh
@@ -0,0 +1,27 @@
+BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+PARSE_MODE="Markdown"
+
+if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
+    build_status="failed"
+else
+    build_status="succeeded"
+fi
+
+
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=${PARSE_MODE}
+}
+
+
+send_msg "
+-------------------------------------
+Travis build *${build_status}!*
+\`Repository:  ${TRAVIS_REPO_SLUG}\`
+\`Branch:      ${TRAVIS_BRANCH}\`
+*Commit Msg:*
+${TRAVIS_COMMIT_MESSAGE}
+[Job Log here](${TRAVIS_JOB_WEB_URL})
+--------------------------------------
+"
